0f3640ae83064d1304195235e166d3a9108a851b
jisoochoi7561 <jisoochoi7561@gmail.com>
2022-06-22 21:03:39 +0900

    temp

--- sqldocker/html/db_setup.php
+++ sqldocker/html/db_setup.php
@@ -10,7 +10,7 @@ $mysql_charset = 'UTF8';
 $conn = new mysqli($mysql_hostname,$mysql_username,$mysql_password,$mysql_database,$mysql_port,$mysql_charset);
 
 if ($conn->connect_errno){
-    echo '연결실패';
+    echo '연결실패 : '.$conn->connect_error.'';
 }
 echo 'hello world';
 ?>
--- sqldocker/html/query.php
+++ sqldocker/html/query.php
@@ -3,6 +3,14 @@ include('db_setup.php');
 
 $results = array();
 
-$results = $conn->query("select * from board");
+$result = $conn->query("select * from dummy");
+
+while ($row = $result->fetch_array(MYSQLI_ASSOC)){
+    $results[] = $row;
+}
+header('Content-type: application/json');
+echo json_encode($results,JSON_NUMERIC_CHECK);
+
+$conn->close();
 
 ?>
