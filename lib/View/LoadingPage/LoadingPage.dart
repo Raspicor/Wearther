c2117dd40d7ecff5b2da9fe521772b22e432541c
HyeonJunSong <music9801@naver.com>
2022-06-22 20:54:10 +0900

    screenUtilInit

---
+++ lib/View/LoadingPage/LoadingPage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoadingPage extends StatelessWidget {
+  const LoadingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "LoadingPage",
+        )
+      )
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+
+import 'View/LoadingPage/LoadingPage.dart';
 
 void main() {
   runApp(const WeartherApp());
@@ -9,6 +13,22 @@ class WeartherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ScreenUtilInit(
+      designSize: Size(360,800),
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Wearther',
+          // theme: ThemeData(
+          //     fontFamily: 'master'
+          // ),
+          initialRoute: "/Load",
+          routingCallback: (route) {},
+          getPages: [
+            GetPage(name: "/Load", page: () => LoadingPage()),
+          ],
+        );
+      }
+    );
   }
 }
