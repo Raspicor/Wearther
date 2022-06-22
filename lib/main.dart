a7c0e974b4c793ac36e27fc34a76d5cff48856a7
HyeonJunSong <music9801@naver.com>
2022-06-22 19:57:38 +0900

    main frame

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WeartherApp());
+}
+
+class WeartherApp extends StatelessWidget {
+  const WeartherApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
